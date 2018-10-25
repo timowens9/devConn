f3180a5538e6f91a817f5922065058588827db0e
Tim <timowens9@gmail.com>
2018-10-25 13:18:26 -0500

    MongoDB server created

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI:
+    'mongodb://dev:pizza9@ds241723.mlab.com:41723/dev'
+};
--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//Database configuration
+const db = require('./config/keys.js').mongoURI;
+
+//Connect to MongoDB
+mongoose
+    .connect(
+        db,
+        { useNewUrlParser: true})
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World'));
 
