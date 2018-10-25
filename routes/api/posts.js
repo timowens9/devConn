da350bdb00e3fe9e2f65b36e007610b718f93e9b
Tim <timowens9@gmail.com>
2018-10-25 14:05:32 -0500

    Route files created

---
+++ routes/api/posts.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+
+// @route   GET api/posts/test
+// @desc    Tests posts route
+// @access  Public route
+router.get('/test', (req, res) => res.json({msg: "Post Works"}));
+
+module.exports = router;
