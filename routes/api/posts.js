5bb8372b3b18c598190105067015b7642055a32f
Tim <timowens9@gmail.com>
2018-11-07 14:47:59 -0600

    Added functionality to get and delete posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,9 @@ const passport = require('passport');
 // Post model
 const Post = require('../../models/Post');
 
+// Profile model
+const Profile = require('../../models/Profile');
+
 // Validation
 const validatePostInput = require('../../validation/post');
 
@@ -14,10 +17,51 @@ const validatePostInput = require('../../validation/post');
 // @access  Public route
 router.get('/test', (req, res) => res.json({msg: "Post Works"}));
 
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public
+router.get('/', (req, res) => {
+    Post.find()
+        .sort({ date: -1 })
+        .then(posts => res.json(posts))
+        .catch(err => res.status(404).json({ nopostsfound: 'No posts found' }));
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Public
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => res.json(post))
+        .catch(err => res.status(404).json({ nopostsfound: 'No post found' }));
+});
+
+// @route   DELETE api/posts/:id
+// @desc    Delete posts by id
+// @access  Private
+router.delete('/:id', passport.authenticate('jwt', {session: false}), (req, res) => {
+    Profile.findOne({user: req.user.id})
+        .then(profile => {
+            Post.findById(req.params.id)
+                .then(post => {
+                    // Check for post owner
+                    if(post.user.toString() !== req.user.id){
+                        return res.status(401).json({notauthorized: 'User not authorized'})
+
+                    }
+                    //Delete
+                    post.remove().then(() => res.json({success: true}));
+                })
+                .catch(err => res.status(404).json({postnotfound: 'No post found'}));
+        })
+});
+
+
+
+
 // @route   POST api/posts
 // @desc    Create posts
 // @access  Private
-
 router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
     const {errors, isValid} = validatePostInput(req.body);
     // Check Validation
